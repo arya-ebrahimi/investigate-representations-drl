82ce91d160fa547bd8633006fc12bd95a56fee82
Arya-Ebrahimi <aryaebrahimi3000@mail.um.ac.ir>
2023-04-12 18:03:54 +0430

    constructing the representation network

--- core/agents/network.py
+++ core/agents/network.py
@@ -8,5 +8,16 @@ class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
         
-        self.conv1 = nn.Conv2d(3, 32, 3)
-        self.conv2 = nn.Conv2d(32, 16, )
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=2)
+        self.conv2 = nn.Conv2d(32, 16, 3, padding=1)
+        self.fc1 = nn.Linear(8*8*16, 32)
+        
+        self.fta = FTA(tiles=20, bound_low=-2, bound_high=+2, eta=0.4, input_dim=32)
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = torch.flatten(x)
+        x = self.fta(self.fc1(x))
+        
+        return x
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import numpy as np
 import gymnasium as gym
 from PIL import Image
+from core.agents.network import Network
+import torch
+
+net = Network()
+print(net)
 
 
-DOWN = 0
-RIGHT = 1
-UP = 2
-LEFT = 3
+input = torch.randn(3, 15, 15)
+out = net(input)
+print(out)
+
+# DOWN = 0
+# RIGHT = 1
+# UP = 2
+# LEFT = 3
 
 
-env = gym.make('core:MazEnv-v0')
-env.reset()
+# env = gym.make('core:MazEnv-v0')
+# env.reset()
 
 
-image = Image.fromarray(env.get_state_image(), 'RGB')
-image.save('img.png')
+# image = Image.fromarray(env.get_state_image(), 'RGB')
+# image.save('img.png')
 
